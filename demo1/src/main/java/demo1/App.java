dcf23f876dacae4088c43b192eb8a6bfb4b1b143
PR20171343 <PR20171343@APL-CZPB403>
2020-12-11 09:59:28 +0530

    commit 3

--- demo1/src/main/java/demo1/App.java
+++ demo1/src/main/java/demo1/App.java
@@ -9,7 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Calc c=new Calc();
-        System.out.println("sum:"+c.add(2, 3));
+    	
+       // Calc c=new Calc();
+       // logger.log("My Message");
+        //System.out.println("sum:"+c.add(2, 3));
     }
 }
--- demo1/src/main/java/demo1/Calc.java
+++
@@ -1,8 +0,0 @@
-package demo1;
-
-public class Calc {
-
-	public int add(int a, int b) {
-		return a+b;
-	}
-}
